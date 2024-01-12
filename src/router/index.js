4b73e97a5541a646a6b1f8d52d2571178c25a6bf
Shuashuaa <jovijoshuatania027@gmail.com>
2024-01-12 22:59:50 +0800

    011224_ROUTER

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+import amIResponsive from '../components/amIResponsive.vue'
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+const router = new createRouter({
+	history: createWebHistory(process.env.BASE_URL),
+	mode: 'history',
+	routes: [
+		{
+			path: '/amiresponsive',
+			name: 'amiresponsive',
+			component: amIResponsive,
+			meta: { title: 'Am I Responsive?' },//##
+		},
+	],
+
+});
 
 export default router
