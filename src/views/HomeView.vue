08329ed806131ffee4a70f7da2e518334b3ffe78
Shuashuaa <jovijoshuatania027@gmail.com>
2024-01-15 21:31:32 +0800

    011524_HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/amIResponsive.vue'
 
 export default {
   name: 'HomeView',
