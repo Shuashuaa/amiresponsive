4c85eb5de0ebf134b928a1b77aabfb00a7a78e07
Shuashuaa <jovijoshuatania027@gmail.com>
2024-01-17 19:54:18 +0800

    011724_STABLE_ERRORVUE_ROUTER

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import amIResponsive from '../components/amIResponsive.vue'
+import Error from '../components/Error.vue'
 
 const router = new createRouter({
 	history: createWebHistory(process.env.BASE_URL),
@@ -12,6 +13,13 @@ const router = new createRouter({
 			component: amIResponsive,
 			meta: { title: 'Am I Responsive?' },//##
 		},
+		{
+			path: '/:catchAll(.*)',
+			name: 'error',
+			component: Error,
+			meta: { title: "404 | Isekai'd out" },//##
+		},
+		
 	],
 
 });
