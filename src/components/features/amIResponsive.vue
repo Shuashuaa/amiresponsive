5ec7c91c2e2554710ab84486712186118ba23175
Shuashuaa <jovijoshuatania027@gmail.com>
2024-04-08 22:14:13 +0800

    040824_sizing_fix

--- src/components/features/amIResponsive.vue
+++ src/components/features/amIResponsive.vue
@@ -19,14 +19,14 @@
 
             <v-card id="visual" class="justify-center d-flex" style="position: relative;" width="900" height="850" elevation="0" >
 
-                <iframe :src="defaultSource" class="iframe-pc" frameborder="0" width="1600" height="900" style="position: absolute; z-index: 2; right: -305px; bottom: 98px; "></iframe>
-                <iframe :src="defaultSource" class="iframe-mobile" frameborder="0" width="350" height="600" style="position: absolute; z-index: 4; left: 105px; bottom: -155px;"></iframe>
-                <iframe :src="defaultSource" class="iframe-laptop" frameborder="0" width="1280" height="802" style="position: absolute; z-index: 3; right: -415px; bottom: -205px;"></iframe>
+                <iframe :src="defaultSource" class="iframe-pc" frameborder="0" width="1600" height="900" style="position: absolute; z-index: 2; right: -305px; bottom: 100px; "></iframe>
+                <iframe :src="defaultSource" class="iframe-mobile" frameborder="0" width="330" height="610" style="position: absolute; z-index: 4; left: 115px; bottom: -160px;"></iframe>
+                <iframe :src="defaultSource" class="iframe-laptop" frameborder="0" width="1280" height="802" style="position: absolute; z-index: 3; right: -375px; bottom: -205px;"></iframe>
                 <iframe :src="defaultSource" class="iframe-tablet" frameborder="0" width="610" height="800" style="position: absolute; z-index: 4; left: -190px; bottom: -205px;"></iframe>
                     
                 <v-img src="/amIResponsive/desktop.png" width="860" style="position: absolute; right: 70px; top: 30px; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"></v-img>
                 <v-img src="/amIResponsive/phone.png" width="300" height="280" style="position: absolute; left: 130px; bottom: 5px; z-index: 2; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"></v-img>
-                <v-img src="/amIResponsive/laptop.png" width="550" style="position: absolute; right: -50px; bottom: 20px; z-index: 2; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"></v-img>
+                <v-img src="/amIResponsive/laptop.png" width="550" style="position: absolute; right: -10px; bottom: 20px; z-index: 2; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"></v-img>
                 <v-img src="/amIResponsive/tablet.png" width="350" style="position: absolute; left: -60px; bottom: 20px; z-index: 3; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"></v-img>
                 
             </v-card>
@@ -88,6 +88,9 @@ export default {
         }
 
         const scrollToContent = () => {
+            if(searchedSource.value == ''){
+                return
+            }
             const sampleElement = document.getElementById("visual");
             sampleElement.scrollIntoView({ behavior: "smooth" });
         }
