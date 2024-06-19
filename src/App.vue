ebc75365b4ef0f5794c2039e30b9985a596ee03d
Shuashuaa <jovijoshuatania027@gmail.com>
2024-06-19 20:21:47 +0800

    061924

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,21 @@
 <template>
-	<v-app id="inspire">
-        <v-main>
-            <router-view/>
-        </v-main>
-
-	</v-app>
-    
+	<div class="App">
+		<h1 style="font-size: 77px;">hello there!.</h1>
+	</div>
+	
+	<RouterView/>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'App',
+	components: {}
+});
+
+</script>
+
 <style>
-	#app{
-        background-color: #f8f8f8;
-    }
-    .title{
-        color: #343a40;
-        font-family: 'Montserrat', sans-serif;
-        font-weight: 900;
-        font-size: 20px;
-    }
-    .text{
-        color: #343a40;
-        font-family: 'Montserrat', sans-serif;
-        font-weight: 400;
-        font-size: 15px;
-    }
-    .nav-items:hover{
-        text-decoration: underline;
-    }
 
-    .v-application .ma-2{
-        margin: 0 !important;
-    }
-    .v-navigation-drawer{
-        /* top: 48px !important; */
-        height: 100% !important;
-    }
-    .v-toolbar{
-        top: 0 !important;
-    }
-    /* padding && margin errors are here */
-    .v-main{ 
-        padding: 0px !important;
-    }
-
-    .mobile-drawer{
-        width: 100% !important;
-    }
-    .mobile-items{
-        padding-top: 20px; 
-        padding-bottom: 20px;
-        margin: 0 !important;
-        color: black;
-    }
-    .mobile-items:hover{
-        background-color: #afe2ff;
-    }
-    .contact{
-        background-color: black;
-        color: white;
-    }
-
-    /* @media screen 
-    and (min-device-width: 1200px) 
-    and (max-device-width: 1600px) {  */
-        /*  */
-        
-    /* } */
-
-    /* @media (-webkit-device-pixel-ratio: 1.25) {
-        
-    } */
 </style>
