2faf7462135eb4a4e4aa14b4637a6fce44817098
Shuashuaa <jovijoshuatania027@gmail.com>
2024-06-22 17:17:44 +0800

    062224

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,17 +1,15 @@
 <template>
-    <div style="display: flex; justify-content: center; position: relative; width: 900px; height: 850px;">
+    <div style="display: flex; justify-content: center; background-color: blue;">
+        <div style="display: flex; justify-content: center; position: relative; width: 900px; height: 850px; background-color: red;">
 
-        <iframe :src="defaultSource" class="iframe-pc" frameborder="0" width="1600" height="900" style="position: absolute; z-index: 2; right: -305px; bottom: 100px; "></iframe>
-        <iframe :src="defaultSource" class="iframe-mobile" frameborder="0" width="330" height="610" style="position: absolute; z-index: 4; left: 115px; bottom: -160px;"></iframe>
-        <iframe :src="defaultSource" class="iframe-laptop" frameborder="0" width="1280" height="802" style="position: absolute; z-index: 3; right: -375px; bottom: -205px;"></iframe>
-        <iframe :src="defaultSource" class="iframe-tablet" frameborder="0" width="610" height="800" style="position: absolute; z-index: 4; left: -190px; bottom: -205px;"></iframe>
+            <iframe :src="defaultSource" class="iframe-pc" frameborder="0" width="1600" height="900" style="position: absolute; z-index: 2; right: -305px; bottom: 100px; "></iframe>
+            <iframe :src="defaultSource" class="iframe-mobile" frameborder="0" width="330" height="610" style="position: absolute; z-index: 4; left: 115px; bottom: -160px;"></iframe>
+            <iframe :src="defaultSource" class="iframe-laptop" frameborder="0" width="1280" height="802" style="position: absolute; z-index: 3; right: -375px; bottom: -205px;"></iframe>
+            <iframe :src="defaultSource" class="iframe-tablet" frameborder="0" width="610" height="800" style="position: absolute; z-index: 4; left: -190px; bottom: -205px;"></iframe>
 
-        <img src="../assets/imgs/desktop.png" width="860" style="position: absolute; right: 70px; top: 30px; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
-        <img src="../assets/imgs/phone.png" width="300" height="280" style="position: absolute; left: 130px; bottom: 5px; z-index: 2; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
-        <img src="../assets/imgs/laptop.png" width="550" style="position: absolute; right: -10px; bottom: 20px; z-index: 2; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
-        <img src="../assets/imgs/tablet.png" width="350" style="position: absolute; left: -60px; bottom: 20px; z-index: 3; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
-
+        </div>
     </div>
+    
 </template>
 
 <script lang="ts">
@@ -24,27 +22,3 @@ export default{
     },
 }
 </script>
-
-<style>
-.iframe-pc{
-    transform: scale(0.4150);
-    -webkit-transform: scale(0.4150);
-    -o-transform: scale(0.4150);
-    -ms-transform: scale(0.4150);
-    -moz-transform: scale(0.4150);
-}
-.iframe-laptop{
-    transform: scale(0.277);
-    -webkit-transform: scale(0.320);
-    -o-transform: scale(0.320);
-    -ms-transform: scale(0.320);
-    -moz-transform: scale(0.320);
-}
-.iframe-tablet, .iframe-mobile{
-    transform: scale(0.4181);
-    -webkit-transform: scale(0.3181);
-    -o-transform: scale(0.3181);
-    -ms-transform: scale(0.3181);
-    -moz-transform: scale(0.3181);
-}
-</style>
