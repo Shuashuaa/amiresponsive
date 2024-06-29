93ef74a0ec78349abb2ca27beab29d08f7774731
Shuashuaa <jovijoshuatania027@gmail.com>
2024-06-30 07:41:07 +0800

    063024

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="justify-center md:flex lg:flex xl:flex">
         <div class="hidden md:block lg:block xl:block" style="position: relative; width: 900px; height: 850px; ">
-            <iframe :src="link" class="hidden md:block lg:block xl:block iframe-pc" frameborder="0" width="1600" height="900" style="position: absolute; z-index: 2; right: -305px; bottom: 100px; "></iframe>
+            <iframe :src="link" class="hidden md:block lg:block xl:block iframe-pc" frameborder="0" width="1600" height="900" style="position: absolute; z-index: 2; right: -330px; bottom: 100px; "></iframe>
             <iframe :src="link" class="hidden md:block lg:block xl:block iframe-mobile" frameborder="0" width="330" height="610" style="position: absolute; z-index: 4; left: 115px; bottom: -160px;"></iframe>
             <iframe :src="link" class="hidden md:block lg:block xl:block iframe-laptop" frameborder="0" width="1280" height="802" style="position: absolute; z-index: 3; right: -375px; bottom: -205px;"></iframe>
             <iframe :src="link" class="hidden md:block lg:block xl:block iframe-tablet" frameborder="0" width="610" height="800" style="position: absolute; z-index: 4; left: -190px; bottom: -205px;"></iframe>
 
-            <img class="hidden md:block lg:block xl:block" src="../assets/imgs/desktop.png" width="860" style="position: absolute; right: 70px; top: 30px; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
+            <img class="hidden md:block lg:block xl:block" src="../assets/imgs/desktop.png" width="860" style="position: absolute; right:45px; top: 30px; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
             <img class="hidden md:block lg:block xl:block" src="../assets/imgs/phone.png" width="300" height="280" style="position: absolute; left: 130px; bottom: 5px; z-index: 2; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
             <img class="hidden md:block lg:block xl:block" src="../assets/imgs/laptop.png" width="550" style="position: absolute; right: -10px; bottom: 20px; z-index: 2; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
             <img class="hidden md:block lg:block xl:block" src="../assets/imgs/tablet.png" width="350" style="position: absolute; left: -60px; bottom: 20px; z-index: 3; -webkit-filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.3));"/>
