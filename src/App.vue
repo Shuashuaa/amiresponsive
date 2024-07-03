e6ec469650840ade65e0ecdbad1ea89e71d5ba5a
Shuashuaa <jovijoshuatania027@gmail.com>
2024-07-03 22:32:00 +0800

    070324

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,25 @@
     <div class="App" id="gradient">
         
         <div id="visual" class="flex justify-between items-center p-5">
-            <img width="45" draggable="false" src="./assets/imgs/am i responsive.png" alt="">
+            <svg width="50" height="50" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_i_5_15)">
+                <path d="M17.56 74V18H44.52C49.7467 18 54.2533 18.8533 58.04 20.56C61.88 22.2667 64.84 24.72 66.92 27.92C69 31.0667 70.04 34.8 70.04 39.12C70.04 43.44 69 47.1733 66.92 50.32C64.84 53.4133 61.88 55.7867 58.04 57.44C54.2533 59.0933 49.7467 59.92 44.52 59.92H28.04L36.44 52.16V74H17.56ZM51.08 74L37.32 53.6H57.32L71.24 74H51.08ZM36.44 54.24L28.04 45.68H43.32C45.9333 45.68 47.8533 45.0933 49.08 43.92C50.36 42.7467 51 41.1467 51 39.12C51 37.0933 50.36 35.4933 49.08 34.32C47.8533 33.1467 45.9333 32.56 43.32 32.56H28.04L36.44 24V54.24Z" fill="#262829"/>
+                </g>
+                <path d="M18.5463 69.1979L25.5614 57.0822L30.6481 59.4729L27.8644 55.1802C28.2604 54.177 28.6224 53.3389 28.9504 52.666C29.3051 51.9469 29.6161 51.3566 29.8833 50.8951C31.6471 47.8489 33.768 45.41 36.2459 43.5785C38.77 41.7736 41.5759 40.4401 44.6635 39.5779L37.0262 33.7691L32.1073 26.2989C35.1174 21.6324 38.7469 18.5571 42.9958 17.0731C47.2446 15.589 51.9027 16.0058 56.97 18.3236C57.3199 18.4645 57.6332 18.6152 57.9102 18.7755C58.1871 18.9358 58.464 19.0962 58.7409 19.2565C60.6333 20.3522 62.1345 21.5912 63.2446 22.9735C64.4276 24.3364 65.7017 25.9677 67.0668 27.8675C69.3186 30.8969 70.6643 33.8947 71.1038 36.8608C71.6163 39.8076 70.9238 42.9194 69.0263 46.1964C67.4763 48.8734 65.4988 51.1179 63.0937 52.93C60.6887 54.7422 57.6083 56.0708 53.8526 56.916C51.7926 57.3872 49.8989 58.2628 48.1716 59.543C46.4711 60.777 45.1131 62.2709 44.0975 64.0248C43.8837 64.394 43.6663 64.8228 43.4452 65.311C43.2241 65.7993 42.9787 66.4892 42.709 67.3807L39.2948 65.7736L42.1186 69.997L35.1035 82.1127L23.4651 76.6681L18.5463 69.1979ZM37.2509 58.7663C37.5935 57.8553 38.0053 56.9845 38.4863 56.1537C39.582 54.2613 41.0069 52.652 42.7609 51.3257C44.561 50.0261 46.5506 49.1444 48.7297 48.6805C52.3858 47.9009 55.3799 46.6148 57.7121 44.8221C60.0443 43.0294 61.9453 40.8638 63.4151 38.3253C65.4462 34.8175 66.0014 31.4105 65.0809 28.1042C64.2332 24.7785 61.9632 22.0467 58.2708 19.9088C57.9939 19.7484 57.717 19.5881 57.4401 19.4277C57.2093 19.2941 56.9421 19.1702 56.6384 19.056C51.7824 16.799 47.3162 16.37 43.2399 17.7691C39.2364 19.1487 35.8632 21.941 33.1203 26.1459L40.9654 32.0749C44.0532 27.6999 47.3619 26.3494 50.8915 28.0234L51.5146 28.3842C52.807 29.1324 53.6219 30.0665 53.9596 31.1864C54.2972 32.3063 54.1453 33.4202 53.5039 34.5279C52.7556 35.8202 51.7535 36.9655 50.4975 37.9639C49.3144 38.9429 47.8386 39.629 46.0701 40.0225C42.8901 40.8313 39.9555 42.1211 37.2662 43.8919C34.6231 45.6894 32.3929 48.1574 30.5757 51.296C29.9877 52.3114 29.4022 53.4822 28.8191 54.8086L37.2509 58.7663ZM52.95 34.2072C51.9346 33.6192 50.7989 33.8245 49.543 34.8228C48.287 35.8212 47.0115 37.3321 45.7166 39.3556C47.0552 38.9597 48.3719 38.3355 49.6667 37.4829C51.0077 36.657 52.1022 35.5651 52.95 34.2072ZM25.8929 58.1062L19.6394 68.9064L29.813 73.6877L36.0664 62.8875L25.8929 58.1062Z" fill="#F5F5F5"/>
+                <defs>
+                <filter id="filter0_i_5_15" x="17.56" y="18" width="53.68" height="60" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                <feOffset dy="4"/>
+                <feGaussianBlur stdDeviation="2"/>
+                <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                <feBlend mode="normal" in2="shape" result="effect1_innerShadow_5_15"/>
+                </filter>
+                </defs>
+            </svg>
+
             <div class="flex gap-4">
                 <button class="cursor-default text-center p-2 pb-3">
                     <svg class="size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" >
@@ -42,7 +60,7 @@
                 <h1 class="justify-content center text-4xl font-black text-[#343a40]">Am I Responsive?</h1>
                 <p>is a tool to check the responsiveness of websites without using an actual gadgets. bu.. but not for prod!.</p>
                 <input v-model="searchedSource" type="text" class="hidden md:block lg:block xl:block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset sm:text-sm sm:leading-6" placeholder="ex. https://nuxt.com/?uwu">
-                <p class="hidden md:block lg:block xl:block text-sm"><span class="underline cursor-pointer" @click="searchedSource = 'https://nuxt.com/?uwu', getAddress('https://nuxt.com/?uwu'), scrollToContent()">https://nuxt.com/?uwu</span> or just <span class="underline cursor-pointer" @click="searchedSource = 'nuxt.com/?uwu', getAddress('nuxt.com/?uwu'), scrollToContent()">nuxt.com/?uwu</span> and for local development, please refer to this link <a style="text-decoration: underline;" href="https://github.com/Shuashuaa/Amiresponsive/blob/main/README.md#Run-it!">README.MD</a>.</p>
+                <p class="hidden md:block lg:block xl:block text-sm"><span class="underline cursor-pointer" @click="searchedSource = 'https://nuxt.com/?uwu', getAddress('https://nuxt.com/?uwu'), scrollToContent()">https://nuxt.com/?uwu</span> or just <span class="underline cursor-pointer" @click="searchedSource = 'nuxt.com/?uwu', getAddress('nuxt.com/?uwu'), scrollToContent()">nuxt.com/?uwu</span> and for local development, please refer to this link <a style="text-decoration: underline;" href="https://github.com/Shuashuaa/Amiresponsive/blob/main/README.md#run-it">README.MD</a>.</p>
                 <button @click="scrollToContent(), getAddress(searchedSource)" class="hidden md:block lg:block xl:block w-full h-10 text-center font-mono font-semibold gap-2 items-center border text-white bg-[#01b4d2] border-slate-300 rounded-lg px-4 py-1 hover:bg-blue-100 hover:shadow-lg transition delay-75 duration-300 ease-in-out">
                     Go flex
                 </button>
@@ -75,7 +93,26 @@
                 </div>
             </div>
             <div id="footer" class="flex justify-between items-center">
-                <img width="90" draggable="false" src="./assets/imgs/am i responsive.png" alt="">
+                <!-- <img width="90" draggable="false" src="./assets/imgs/am i responsive.png" alt=""> -->
+                <svg width="75" height="75" viewBox="0 0 55 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g filter="url(#filter0_i_8_31)">
+                    <path d="M0.559998 58V2H27.52C32.7467 2 37.2533 2.8533 41.04 4.56C44.88 6.2667 47.84 8.72 49.92 11.92C52 15.0667 53.04 18.8 53.04 23.12C53.04 27.44 52 31.1733 49.92 34.32C47.84 37.4133 44.88 39.7867 41.04 41.44C37.2533 43.0933 32.7467 43.92 27.52 43.92H11.04L19.44 36.16V58H0.559998ZM34.08 58L20.32 37.6H40.32L54.24 58H34.08ZM19.44 38.24L11.04 29.68H26.32C28.9333 29.68 30.8533 29.0933 32.08 27.92C33.36 26.7467 34 25.1467 34 23.12C34 21.0933 33.36 19.4933 32.08 18.32C30.8533 17.1467 28.9333 16.56 26.32 16.56H11.04L19.44 8V38.24Z" fill="#262829"/>
+                    </g>
+                    <path d="M1.5463 53.1979L8.5614 41.0822L13.6481 43.4729L10.8644 39.1802C11.2604 38.177 11.6224 37.3389 11.9504 36.666C12.3051 35.9469 12.6161 35.3566 12.8833 34.8951C14.6471 31.8489 16.768 29.41 19.2459 27.5785C21.77 25.7736 24.5759 24.4401 27.6635 23.5779L20.0262 17.7691L15.1073 10.2989C18.1174 5.6324 21.7469 2.5571 25.9958 1.0731C30.2446 -0.411003 34.9027 0.00579858 39.97 2.3236C40.3199 2.4645 40.6332 2.6152 40.9102 2.7755C41.1871 2.9358 41.464 3.0962 41.7409 3.2565C43.6333 4.3522 45.1345 5.5912 46.2446 6.9735C47.4276 8.3364 48.7017 9.9677 50.0668 11.8675C52.3186 14.8969 53.6643 17.8947 54.1038 20.8608C54.6163 23.8076 53.9238 26.9194 52.0263 30.1964C50.4763 32.8734 48.4988 35.1179 46.0937 36.93C43.6887 38.7422 40.6083 40.0708 36.8526 40.916C34.7926 41.3872 32.8989 42.2628 31.1716 43.543C29.4711 44.777 28.1131 46.2709 27.0975 48.0248C26.8837 48.394 26.6663 48.8228 26.4452 49.311C26.2241 49.7993 25.9787 50.4892 25.709 51.3807L22.2948 49.7736L25.1186 53.997L18.1035 66.1127L6.4651 60.6681L1.5463 53.1979ZM20.2509 42.7663C20.5935 41.8553 21.0053 40.9845 21.4863 40.1537C22.582 38.2613 24.0069 36.652 25.7609 35.3257C27.561 34.0261 29.5506 33.1444 31.7297 32.6805C35.3858 31.9009 38.3799 30.6148 40.7121 28.8221C43.0443 27.0294 44.9453 24.8638 46.4151 22.3253C48.4462 18.8175 49.0014 15.4105 48.0809 12.1042C47.2332 8.7785 44.9632 6.0467 41.2708 3.9088C40.9939 3.7484 40.717 3.5881 40.4401 3.4277C40.2093 3.2941 39.9421 3.1702 39.6384 3.056C34.7824 0.798998 30.3162 0.369998 26.2399 1.7691C22.2364 3.1487 18.8632 5.941 16.1203 10.1459L23.9654 16.0749C27.0532 11.6999 30.3619 10.3494 33.8915 12.0234L34.5146 12.3842C35.807 13.1324 36.6219 14.0665 36.9596 15.1864C37.2972 16.3063 37.1453 17.4202 36.5039 18.5279C35.7556 19.8202 34.7535 20.9655 33.4975 21.9639C32.3144 22.9429 30.8386 23.629 29.0701 24.0225C25.8901 24.8313 22.9555 26.1211 20.2662 27.8919C17.6231 29.6894 15.3929 32.1574 13.5757 35.296C12.9877 36.3114 12.4022 37.4822 11.8191 38.8086L20.2509 42.7663ZM35.95 18.2072C34.9346 17.6192 33.7989 17.8245 32.543 18.8228C31.287 19.8212 30.0115 21.3321 28.7166 23.3556C30.0552 22.9597 31.3719 22.3355 32.6667 21.4829C34.0077 20.657 35.1022 19.5651 35.95 18.2072ZM8.8929 42.1062L2.6394 52.9064L12.813 57.6877L19.0664 46.8875L8.8929 42.1062Z" fill="white"/>
+                    <defs>
+                    <filter id="filter0_i_8_31" x="0.559998" y="2" width="53.68" height="60" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                    <feOffset dy="4"/>
+                    <feGaussianBlur stdDeviation="2"/>
+                    <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                    <feBlend mode="normal" in2="shape" result="effect1_innerShadow_8_31"/>
+                    </filter>
+                    </defs>
+                </svg>
+
                 <div class="text-right">
                     <button class="font-mono text-gray-800 font-bold text-md transition delay-75 duration-300 ease-in-out hover:border hover:rounded hover:border-gray-300 hover:shadow-md p-2 ml-5">version 1.2.1.0</button>
                     <div class="flex items-center gap-2">
