e63bcc982620f2c36775a33a5cef7db6d5c86157
Shuashuaa <jovijoshuatania027@gmail.com>
2024-07-06 00:06:19 +0800

    070624

---
+++ src/views/AnimaContent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="area">
+        <ul class="circles">
+            <li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
