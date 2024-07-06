962b787c6f397f7b03a0cec61218e83c864fc371
Shuashuaa <jovijoshuatania027@gmail.com>
2024-07-06 18:05:04 +0800

    070624-hotjar/removed

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import VueHotjar from 'vue-hotjar';
 
 const app = createApp(App)
-app.use(VueHotjar, {
-    id: '5049874', // Replace with your Hotjar tracking ID  
-    isProduction: true,
-    snippetVersion: 6
-});
+
 app.use(createPinia())
 app.use(router)
 
