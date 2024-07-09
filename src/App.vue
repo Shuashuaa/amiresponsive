9c46b1d7379204d379aef000b393750ada6f5729
Shuashuaa <jovijoshuatania027@gmail.com>
2024-07-09 17:23:23 +0800

    070924

--- src/App.vue
+++ src/App.vue
@@ -81,7 +81,7 @@
                     style="background-image: linear-gradient(240deg, rgba(149, 131, 198, 0) 1.46%, rgba(149, 131, 198, 0.8) 40.83%, rgba(149, 131, 198, 0.8) 65.57%, rgba(149, 131, 198, 0) 107.92%);">
                 </div>
                     <div class="hidden md:[857px] lg:block xl:block flex w-[200px]"> 
-                        <a href="https://github.com/Shuashuaa/Amiresponsive">
+                        <a href="https://github.com/Shuashuaa/Amiresponsive?tab=readme-ov-file#amiresponsive">
                            <button class="w-full flex font-mono text-gray-700 gap-2 items-center border border-slate-300 rounded-lg px-4 py-1 hover:bg-blue-100 hover:shadow-lg transition delay-75 duration-300 ease-in-out">
                                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 24 24">
                                     <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.6,5,2.5,9.3,6.9,10.7v-2.3c0,0-0.4,0.1-0.9,0.1c-1.4,0-2-1.2-2.1-1.9 c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1c0.4,0,0.7-0.1,0.9-0.2 c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6c0,0,1.4,0,2.8,1.3 C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3 c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v3.3c4.1-1.3,7-5.1,7-9.5C22,6.1,16.9,1.4,10.9,2.1z"></path>
