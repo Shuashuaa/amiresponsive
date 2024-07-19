ef200f1c29dec36287c83c6f1d3aa6de0bcf28cb
Shuashuaa <jovijoshuatania027@gmail.com>
2024-07-19 23:55:07 +0800

    071924

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@
                     <button class="border border-gray-300 rounded flex items-center justify-center font-mono text-gray-500 text-sm transition delay-75 duration-300 ease-in-out hover:border hover:rounded hover:border-gray-300 hover:shadow-md py-1 px-5 mr-5 gap-2">
                         
                         <div class="border-r border-gray-300 pr-3 flex justify-center items-center">
-                            <svg class="size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray">
+                            <svg class="size-6 hover:fill-yellow-400 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                             </svg>
                             <p class="pl-2">Star</p>   
