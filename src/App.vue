dd765fe4721121a0e7c25e6aa573bb59111bf141
Shuashuaa <jovijoshuatania027@gmail.com>
2024-08-24 12:35:36 +0800

    082424-v2.0.0

--- src/App.vue
+++ src/App.vue
@@ -23,11 +23,11 @@
             </svg>
 
             <div class="group flex gap-4">
-                <button class="cursor-default text-center p-2 pb-3">
+                <!-- <button class="cursor-default text-center p-2 pb-3">
                     <svg class="size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray" >
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
                     </svg>
-                </button>
+                </button> -->
 
                 <div class="h-[40px] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-25 dark:via-neutral-400"></div>
                 
