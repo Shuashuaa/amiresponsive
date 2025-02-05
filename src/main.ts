37a198af619d5895013950c5455bb29c5e2a73d9
Shuashuaa <jovijoshuatania027@gmail.com>
2025-02-05 20:53:32 +0800

    020525

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,11 @@ import './assets/main.css'
 import './assets/styles.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
