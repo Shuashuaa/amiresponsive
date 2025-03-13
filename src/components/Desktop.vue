fc65fd97d54aa23799581998cca86bdbd230eae4
Shuashuaa <jovijoshuatania027@gmail.com>
2025-03-13 18:47:13 +0800

    Refactor components for minimal Vue 3 approach and improved structure

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -1,7 +1,10 @@
-<script setup>
-
+<script setup lang="ts">
+defineProps({
+    link: String
+});
 </script>
 
 <template>
-    
+    <iframe :src="link" class="hidden md:[857px] lg:block xl:block iframe-pc absolute z-10 right-[-330px] bottom-[100px]" frameborder="0" width="1600" height="900"></iframe>
+    <img class="hidden md:[857px] lg:block xl:block absolute right-[45px] top-[30px] filter drop-shadow-[2px_2px_2px_rgba(0,0,0,0.3)]" src="../assets/imgs/desktop.png" width="860"/>
 </template>
--- src/components/Mobile.vue
+++ src/components/Mobile.vue
@@ -1,7 +1,10 @@
-<script setup>
-
+<script setup lang="ts">
+defineProps({
+    link: String
+});
 </script>
 
 <template>
-
+    <iframe :src="link" class="hidden md:[857px] lg:block xl:block iframe-mobile absolute z-10 left-[115px] bottom-[-160px]" frameborder="0" width="330" height="610"></iframe>
+    <img class="hidden md:[857px] lg:block xl:block absolute left-[130px] bottom-[5px] z-2 filter drop-shadow-[2px_2px_2px_rgba(0,0,0,0.3)]" src="../assets/imgs/phone.png" width="300" height="280"/>
 </template>
